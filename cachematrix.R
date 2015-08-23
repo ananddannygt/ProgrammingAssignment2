acbe5c0d4755f6943493abaeaf27ad6feb809eb8
danwarrior <coorgfav@gmail.com>
2015-08-23 15:56:34 +0530

    added the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+#-----------------------------------------------------------------------------
+# Calculates the inverse of a matrix
+# ------------------------------------
+# Has two functions
+#
+# 1. makeCacheMatrix: This function creates a special "matrix" object 
+#			    that can cache its inverse.
+#
+# 2. cacheSolve: This function computes the inverse of the special "matrix" 
+#		     returned by makeCacheMatrix above. If the inverse has already 
+#		     been calculated (and the matrix has not changed), then the 
+#		     cachesolve should retrieve the inverse from the cache.
+#
+# Assumption: The matrix supplied is always invertible.
+#-----------------------------------------------------------------------------
 
-## Write a short comment describing this function
+# makeCacheMatrix  function creates a special "vector", which is really a list 
+# containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse of the matrix
+# get the value of the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=matrix()){
+	invmat<-NULL
+	
+	set <- function(y){
+		x <<- y
+		invmat <<- NULL
+	}
 
+	get <- function() x
+
+	setinv <- function(inv) invmat <<- inv
+
+	getinv <- function() invmat
+
+	list(set = set, get =get,
+	     setinv = setinv,
+	     getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# cacheSolve function computes the inverse of the matrix.
+# Throws an error if the matrix supplied is not invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	invmat <- x$getinv()
+
+	if(!is.null(invmat)) {
+      	message("getting cached inverse.")
+      	return(invmat)
+      }
+    
+	data <- x$get()
+	
+	invmat <- solve(data)
+
+	x$setinv(invmat)
+
+	invmat
 }
+
+	
